d2380150ef01eeb4870ae799248c86c667392abd
youngmin63 <kym827432@gmail.com>
2025-03-25 21:31:12 +0900

    Create PingController.java

---
+++ src/main/java/com/example/balancetest/controller/PingController.java
@@ -0,0 +1,16 @@
+package com.example.balancetest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Render 서버 깨우기용 Ping API
+ */
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
